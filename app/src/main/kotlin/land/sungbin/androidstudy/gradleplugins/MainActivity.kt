808c95cfd04092f482f851df106b0d83299429e6
jisungbin <ji@sungb.in>
2023-04-28 20:52:43 +0900

    Cleanup

--- app/src/main/kotlin/land/sungbin/androidstudy/gradleplugins/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidstudy/gradleplugins/MainActivity.kt
@@ -1,10 +1,10 @@
 package land.sungbin.androidstudy.gradleplugins
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(TextView(this).apply { text = "안드로이드 과외 자료" })
